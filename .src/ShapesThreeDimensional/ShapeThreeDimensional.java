0e5c73494f0d4603644fc8e160b92b10bdac2079
olea <olea.plum@gmail.com>
2022-06-07 20:24:11 +0200

    calculate prices based on an outer layer and inner material

--- .src/ShapesThreeDimensional/ShapeThreeDimensional.java
+++ .src/ShapesThreeDimensional/ShapeThreeDimensional.java
@@ -1,8 +1,24 @@
 package ShapesThreeDimensional;
 
+import java.util.*;
+
+import Prices.*;
+
 public abstract class ShapeThreeDimensional {
+    private static final PriceDataRetriever retrieverWrapper = new PriceDataRetriever("Materialpreise Oberflaechen.csv", "\t");
+    private static final PriceDataRetriever retrieverFill = new PriceDataRetriever("Materialpreise Fuellung.csv", ";");
+    private static final HashMap<String, Double> wrapperPrices = retrieverWrapper.getRecords();
+    private static final HashMap<String, Double> fillPrices = retrieverFill.getRecords();
+
     abstract public double calculateSurfaceArea();
 
     abstract public double calculateVolume();
+
+    public double calculatePrice(String wrapperMaterial, String fillMaterial) {
+        double priceFill = this.calculateVolume() * fillPrices.get(fillMaterial);
+        double priceWrapper = this.calculateSurfaceArea() * wrapperPrices.get(wrapperMaterial);
+        return priceFill + priceWrapper;
+
+    }
 
 }
